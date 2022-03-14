92da834085e168846c9085c123f6bd1a25c18d0e
aliadil <shah87061@gmail.com>
2022-03-14 21:27:38 +0500

    add frontend

--- covid/admin.py
+++ covid/admin.py
@@ -1,15 +1,48 @@
 from django.contrib import admin
-from .models import State, City,Hospital, Services
+from .models import State, City, Hospital, Services
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 
 @receiver(post_save, sender=Hospital)
 def afterHospitalsace(signal, instance, **kwargs):
-    service = Services(hospital = instance)
+    service = Services(hospital=instance)
     service.save()
 
-admin.site.register(State) 
-admin.site.register(City)
-admin.site.register(Hospital)
-admin.site.register(Services)
+
+# this is how we stand to show fields in the table
+# for services table
+class ServiceAdmin(admin.ModelAdmin):
+    model = Services
+    list_display = ['hospital',
+                    'oxygen_beds',
+                    'oxygen_cylinder',
+                    'ventilator',
+                    ]
+
+    def oxygen_beds(self, object):
+        return f'{object.oxygen_beds_available}/{object.oxygen_beds_totals}'
+
+    def oxygen_cylinder(self, object):
+        return f'{object.oxygen_cylinder_available}/{object.oxygen_cylinder_total}'
+
+    def ventilator(self, object):
+        return f'{object.ventilator_available}/{object.ventilator_total}'
+
+
+# for hospital model
+class HospitalAdmin(admin.ModelAdmin):
+    model = Hospital
+    list_display = ['name', 'phone', 'address', 'city']
+
+
+# for city model
+class CityAdmin(admin.ModelAdmin):
+    model = City
+    list_display = ['name', 'state']
+
+
+admin.site.register(State)
+admin.site.register(City, CityAdmin)
+admin.site.register(Hospital, HospitalAdmin)
+admin.site.register(Services, ServiceAdmin)
--- covid/views.py
+++ covid/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
+from .models import Services, State, City
 
 
 def home(request):
-    return render(request, template_name='covid/index.html')
+    services = Services.objects.all()
+    cities = City.objects.all()
+    states = State.objects.all()
+    context = {
+        'services' : services,
+        'cities' : cities,
+        'states' : states
+    }
+    return render(request, template_name='covid/index.html', context=context)
